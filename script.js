f88a816280b2063d04fa5af98fda39d757e27239
karolk <karolkusper@gmail.com>
2025-04-06 22:59:31 +0200

    Add country auto fill using ip

--- script.js
+++ script.js
@@ -26,39 +26,51 @@ async function fetchAndFillCountries() {
 
 function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
-        .then(response => response.json())
-        .then(data => {
-            const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
-        })
-        .catch(error => {
-            console.error('Błąd pobierania danych z serwera GeoJS:', error);
-        });
-}
-
-function getCountryCode(countryName) {
+      .then(response => response.json())
+      .then(data => {
+        const country = data.country;
+        // Ustaw wartość w polu kraju – przyjmujemy, że zmieniasz select na input z datalist (patrz pkt. 4)
+        const countryInput = document.getElementById('country');
+        countryInput.value = country;
+        // Wywołanie funkcji pobierającej kod kierunkowy
+        getCountryCode(country);
+      })
+      .catch(error => {
+        console.error('Błąd pobierania danych z serwera GeoJS:', error);
+      });
+  }
+  
+  function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-
+  
     fetch(apiUrl)
-    .then(response => {
+      .then(response => {
         if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
+          throw new Error('Błąd pobierania danych');
         }
         return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
+      })
+      .then(data => {
+        // Załóżmy, że korzystamy tylko z pierwszego dopasowania i jednego suffixu
+        const countryCode = data[0].idd.root + data[0].idd.suffixes[0];
+        // Ustawienie wybranego kodu w polu kierunkowym
+        const countryCodeSelect = document.getElementById('countryCode');
+        for (let option of countryCodeSelect.options) {
+          if (option.value === countryCode) {
+            option.selected = true;
+            break;
+          }
+        }
+      })
+      .catch(error => {
         console.error('Wystąpił błąd:', error);
-    });
-}
-
-
-(() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
+      });
+  }
+  
+  // Wywołaj funkcję getCountryByIP przy inicjalizacji formularza
+  (() => {
     document.addEventListener('click', handleClick);
-
     fetchAndFillCountries();
-})()
+    getCountryByIP();
+  })();
+  
